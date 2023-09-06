75740a7f51fe83bc665ffe07e077aef1e90b9cbf
Gerson Quiroa <3005563490101@ingenieria.usac.edu.gt>
2023-09-05 23:55:13 -0600

    Componente para crear evento
    Se creó una página para poder crear un evento, se le agregaron varios field text, select, inputs, entre otros para manejar la información que el organizador desee publicar.

---
+++ frontend/src/pages/CrearEvento/CrearEvento.js
@@ -0,0 +1,270 @@
+import React, { useState } from "react";
+import { styled } from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./CrearEvento.css";
+import {
+  FormControl,
+  Grid,
+  TextField,
+  Button,
+  Autocomplete,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import {
+  DatePicker,
+  LocalizationProvider,
+  TimePicker,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+
+export function CrearEvento() {
+  const [titulo, setTitulo] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [hora, setHora] = useState("");
+  const [duracion, setDuracion] = useState("");
+  const [ubicacion, setUbicacion] = useState("");
+  const [categoria, setCategoria] = useState([]);
+  const [costo, setCosto] = useState([]);
+  const [imagen, setImagen] = useState("");
+  const [formatoEvento, setFormatoEvento] = useState("");
+  const [materialesEdu, setMaterialesEdu] = useState("");
+
+  const crearEvento = () => {
+    console.log(titulo);
+    console.log(descripcion);
+    console.log(fecha);
+    console.log(hora);
+  };
+
+  return (
+    <Container>
+      <ContainerContent component="form">
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          xs={7}
+        >
+          <Grid item xs={4}>
+            <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
+              <TextField
+                id="outlined-basic"
+                label="Título"
+                variant="outlined"
+                onChange={(newValue) => setTitulo(newValue.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 150 }} size="small">
+              <TextField
+                id="outlined-multiline-flexible"
+                label="Descripción"
+                multiline
+                maxRows={4}
+                variant="outlined"
+                onChange={(newValue) => setDescripcion(newValue.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 150 }} size="small">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Fecha"
+                  onChange={(newValue) =>
+                    setFecha(`${newValue.$D}/${newValue.$M + 1}/${newValue.$y}`)
+                  }
+                />
+              </LocalizationProvider>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 150 }} size="small">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <TimePicker
+                  label="Hora"
+                  onChange={(newValue) =>
+                    setHora(`${newValue.$H}:${newValue.$m}`)
+                  }
+                />
+              </LocalizationProvider>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 150 }} size="small">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <TimePicker
+                  label="Duracion (horas)"
+                  views={["minutes", "seconds"]}
+                  format="mm:ss"
+                />
+              </LocalizationProvider>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 150 }} size="small">
+              <TextField
+                id="outlined-basic"
+                label="Ubicacion"
+                variant="outlined"
+                onChange={(newValue) => setTitulo(newValue.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={8}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 150 }} size="small">
+              <Autocomplete
+                multiple
+                id="tags-outlined"
+                options={categorias}
+                getOptionLabel={(option) => option}
+                defaultValue={[categorias[0], categorias[1]]}
+                filterSelectedOptions
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Categorías"
+                    placeholder="Materias"
+                  />
+                )}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
+              <InputLabel htmlFor="outlined-adornment-amount">Costo</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-amount"
+                startAdornment={
+                  <InputAdornment position="start">Q</InputAdornment>
+                }
+                label="Costo"
+                type="number"
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
+              <Button
+                component="label"
+                variant="contained"
+                startIcon={<CloudUploadIcon />}
+                href="#file-upload"
+              >
+                Imagen promocional
+                <input type="file" hidden />
+              </Button>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
+              <InputLabel id="demo-select-small-label">
+                Formato de evento
+              </InputLabel>
+              <Select
+                labelId="demo-select-small-label"
+                id="demo-select-small"
+                value={formatoEvento}
+                label="Formato de evento"
+                onChange={(newValue) => setFormatoEvento(newValue.target.value)}
+              >
+                <MenuItem value={"presencial"}>Presencial</MenuItem>
+                <MenuItem value={"virtual"}>Virtual</MenuItem>
+                <MenuItem value={"hibrido"}>Híbrido</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4}>
+            <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
+              <Button
+                component="label"
+                variant="contained"
+                startIcon={<CloudUploadIcon />}
+                href="#file-upload"
+              >
+                Materiales adicionales
+                <input type="file" hidden />
+              </Button>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Button variant="contained" color="success" onClick={crearEvento}>
+              Success
+            </Button>
+          </Grid>
+        </Grid>
+      </ContainerContent>
+    </Container>
+  );
+}
+
+const categorias = [
+  "Área común",
+  "Ciencia",
+  "Tecnología",
+  "Medicina",
+  "Derecho",
+  "Arquitectura",
+  "Programación",
+  "Sistemas",
+  "Ingeniería",
+  "Finanzas",
+  "Diseño gráfico",
+  "Deporte",
+  "Ocio",
+  "Programación",
+  "Matemática",
+  "Física",
+  "Contabilidad",
+];
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  min-height: 100%;
+  width: 100vh;
+  min-width: 100%;
+  background: rgb(0, 0, 0);
+  background: linear-gradient(
+    30deg,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(43, 43, 43, 1) 43%,
+    rgba(49, 49, 49, 1) 45%,
+    rgba(54, 54, 54, 1) 52%,
+    rgba(124, 124, 124, 1) 100%
+  );
+`;
+
+const VisuallyHiddenInput = styled("input")`
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  white-space: nowrap;
+  width: 1px;
+`;
+
+const ContainerContent = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 70%;
+  min-height: 70%;
+  width: 80%;
+  min-width: 80%;
+  background: white;
+  margin-top: 8rem;
+  border-radius: 0.5rem;
+`;
