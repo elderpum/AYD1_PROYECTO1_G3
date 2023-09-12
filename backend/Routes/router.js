80c6bb1c09cde909a58d14fee92e9a2d888c2c2c
EsdrasToc <69408116+EsdrasToc@users.noreply.github.com>
2023-09-11 23:38:29 -0600

    [ADD] Login endpoint
    Se realizo el endpoint para la autenticacion de cualquiera de los 3 tipos de usuarios.

--- backend/Routes/router.js
+++ backend/Routes/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const routerEstudiante = require ('./routerEstudiantes.js');
 const routerAdmin = require ('./routerAdmin.js');
+const routerAuth = require ('./routerAuth.js');
 
 //Routes Estudiantes
 router.use('/estudiantes', routerEstudiante);
@@ -9,5 +10,7 @@ router.use('/estudiantes', routerEstudiante);
 //Routes Administrador
 router.use('/admin',routerAdmin)
 
+//Routes Auth
+router.use('/auth', routerAuth);
 
 module.exports = router;
