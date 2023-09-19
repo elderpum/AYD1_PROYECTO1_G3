291dbd2bfc39136ee5bc1a2f5856ee69e012274b
Mario <3017701820101@ingenieria.usac.edu.gt>
2023-09-19 11:18:52 -0600

    Cambios en la rutas y Creacion de Card Busqueda
    Se hicieron cambios en las rutas para los organizadores y estudiantes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,19 +24,21 @@ function App() {
           */}
           <Route path="/" element={<InicioSesion />} />
           <Route path="/admin" element={<Administracion />} />
-          <Route path="/main" element={<Main />} />
           <Route path="/registroEstudiante" element={<RegistroEstudiante />} />
           <Route path="/registroOrganizacion" element={<RegistroOrganizacion />} />
-          <Route path="/crear-evento" element={<CrearEvento />} />
-          <Route path="/agregarMaterial" element={<AgregarMaterial />} />
+          {/* RUTAS PARA LOS ESTUDIANTES */}
+          <Route path="/main" element={<Main  isOrganizador={false}/>} />
           <Route path="/ver-material" element={<VerMaterial />} />
           <Route path="/historial-eventos" element={<HistorialEvento />} />
-          <Route path="/misEventos" element={<MisEventos />} />
           <Route path="/buscarEvento" element={<BuscarEventos isOrganizador={false}/>}/>
           <Route path="/foro" element={<Main isOrganizador={false}/>}/>
-
+          {/* RUTAS PARA LOS ORGANIZADORES */}
+          <Route path="/org/main" element={<Main isOrganizador={true}/>} />
           <Route path="/org/buscarEvento" element={<BuscarEventos isOrganizador={true}/>}/>
           <Route path="/org/foro" element={<Main isOrganizador={true}/>}/>
+          <Route path="/crear-evento" element={<CrearEvento />} />
+          <Route path="/misEventos" element={<MisEventos />} />
+          <Route path="/agregarMaterial" element={<AgregarMaterial />} />
         </Routes>
       </BrowserRouter>
     </div>
